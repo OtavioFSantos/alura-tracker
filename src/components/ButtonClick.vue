4fdb04e57fa44ba6e9a6b2aa8eb5667709bee8e7
OtavioFSantos <otaviofs01@gmail.com>
2022-07-01 10:19:55 -0300

    created button component

---
+++ src/components/ButtonClick.vue
@@ -0,0 +1,35 @@
+<template>
+  <button class="button" @click="clicked" :disabled="buttonDisabled">
+    <span class="icon">
+      <i :class="buttonIcon"></i>
+    </span>
+    <span>{{ text }}</span>
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "ButtonClick",
+  emits: ["clicked"],
+  props: {
+    buttonDisabled: {
+      type: Boolean,
+    },
+    buttonIcon: {
+      type: String,
+      required: true,
+    },
+    text: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    clicked(): void {
+      this.$emit("clicked");
+    },
+  },
+});
+</script>
