53a30ebce253bb3c03c9d7281b2195318e024de5
OtavioFSantos <otaviofs01@gmail.com>
2022-07-01 10:21:13 -0300

    created cronometer component

---
+++ src/components/TimePassed.vue
@@ -0,0 +1,24 @@
+<template>
+  <section>
+    <strong>{{ time }}</strong>
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "TimePassed",
+  props: {
+    timeSeconds: {
+      type: Number,
+      default: 0,
+    },
+  },
+  computed: {
+    time(): string {
+      return new Date(this.timeSeconds * 1000).toISOString().substr(11, 8);
+    },
+  },
+});
+</script>
