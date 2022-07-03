2ab91f3ae135deff3c03cb75c21b2ce74e93c6ad
OtavioFSantos <otaviofs01@gmail.com>
2022-07-03 14:03:43 -0300

    component for tasks

---
+++ src/components/TaskComponent.vue
@@ -0,0 +1,31 @@
+<template>
+  <BoxComponent>
+    <div class="columns">
+      <div class="column is-7">{{ task.description || "Generic Task" }}</div>
+      <div class="column">
+        <TimePassed :timeSeconds="task.timeSeconds" />
+      </div>
+    </div>
+  </BoxComponent>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+import TimePassed from "./TimePassed.vue";
+import ITask from "../interface/ITask";
+import BoxComponent from "./BoxComponent.vue";
+
+export default defineComponent({
+  name: "TaskComponent",
+  components: {
+    TimePassed,
+    BoxComponent,
+  },
+  props: {
+    task: {
+      type: Object as PropType<ITask>,
+      required: true,
+    },
+  },
+});
+</script>
