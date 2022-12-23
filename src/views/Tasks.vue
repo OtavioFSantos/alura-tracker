4be0c51826e9fc224a1ab91b8d1fc63601e437a8
OtavioFSantos <otaviofs01@gmail.com>
2022-12-23 00:39:02 -0300

    router basics stablished

---
+++ src/views/Tasks.vue
@@ -0,0 +1,43 @@
+<template>
+    <TaskForm @onSaveTask="saveTask" />
+    <div class="taskList">
+        <TaskComponent
+          v-for="(task, index) in tasks"
+          :key="index"
+          :task="task"
+        />
+        <BoxComponent v-if="emptyList"> No tasks yet! </BoxComponent>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import TaskForm from "../components/TaskForm.vue";
+import TaskComponent from "../components/TaskComponent.vue";
+import ITask from "../interface/ITask";
+import BoxComponent from "../components/BoxComponent.vue";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    TaskForm,
+    TaskComponent,
+    BoxComponent,
+  },
+  data() {
+    return {
+      tasks: [] as ITask[]
+    };
+  },
+  computed: {
+    emptyList(): boolean {
+      return this.tasks.length === 0;
+    },
+  },
+  methods: {
+    saveTask(task: ITask) {
+      this.tasks.push(task);
+    }
+  },
+});
+</script>
