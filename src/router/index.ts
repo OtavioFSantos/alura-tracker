a0fa178bca544c36920d2b2ae02a23556e9626ed
OtavioFSantos <otaviofs01@gmail.com>
2022-12-25 23:20:06 -0300

    add routes to create and edit a project

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,34 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
-import Tasks from '../views/Tasks.vue';
-import Projects from '../views/Projects.vue';
+import Tasks from "../views/Tasks.vue";
+import Projects from "../views/Projects.vue";
+import Form from "../views/projects/Form.vue";
 
 const routes: RouteRecordRaw[] = [
-    {
-        path: '/',
-        name: 'Tasks',
-        component: Tasks
-    },
-    {
-        path: '/projects',
-        name: 'Projects',
-        component: Projects
-    }
-]
+  {
+    path: "/",
+    name: "Tasks",
+    component: Tasks,
+  },
+  {
+    path: "/projects",
+    name: "Projects",
+    component: Projects,
+  },
+  {
+    path: "/projects/new",
+    name: "New project",
+    component: Form,
+  },
+  {
+    path: "/projects/:id",
+    name: "Edit project",
+    component: Form,
+  },
+];
 
 const router = createRouter({
-    history: createWebHashHistory(),
-    routes: routes
-})
+  history: createWebHashHistory(),
+  routes: routes,
+});
 
 export default router;
