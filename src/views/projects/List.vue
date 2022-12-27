570716f5d992a719f66755e4d39edf81e88f837d
OtavioFSantos <otaviofs01@gmail.com>
2022-12-27 15:48:30 -0300

    more organization on projects and lists

---
+++ src/views/projects/List.vue
@@ -0,0 +1,59 @@
+<template>
+  <section>
+    <router-link to="/projects/new" class="button">
+      <span class="icon is-small">
+        <i class="fa fa-plus"></i>
+      </span>
+      <span>New Project</span>
+    </router-link>
+    <table class="table is-fullwidth">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="project in projects" :key="project.id">
+          <td>{{ project.id }}</td>
+          <td>{{ project.name }}</td>
+          <td>
+            <router-link :to="`/projects/${project.id}`" class="button">
+              <span class="icon is-small">
+                <i class="fa fa-pencil-alt"></i>
+              </span>
+            </router-link>
+            <button class="button ml-2 is-danger" @click="remove(project.id)">
+              <span class="icon is-small">
+                <i class="fa fa-trash"></i>
+              </span>
+            </button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent, computed } from "vue";
+import { useStore } from "@/store";
+import { REMOVE_PROJECT } from "@/store/type-mutations";
+
+export default defineComponent({
+  name: "List-component",
+  methods: {
+    remove(id: string) {
+      this.store.commit(REMOVE_PROJECT, id);
+    },
+  },
+  setup() {
+    const store = useStore();
+    return {
+      projects: computed(() => store.state.projects),
+      store,
+    };
+  },
+});
+</script>
