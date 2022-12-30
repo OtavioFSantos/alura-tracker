c2b09a4e64b751baeeef79f9893fecfa4aaac750
OtavioFSantos <otaviofs01@gmail.com>
2022-12-30 20:19:33 -0300

    created different slots for modal

---
+++ src/components/Modal.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="modal" :class="{ 'is-active': display }" v-if="display">
+    <div class="modal-background"></div>
+    <div class="modal-card">
+      <header class="modal-card-head">
+        <slot name="head" />
+      </header>
+      <section class="modal-card-body">
+        <slot name="body" />
+      </section>
+      <footer class="modal-card-foot">
+        <slot name="foot" />
+      </footer>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "ModalComponent",
+  props: {
+    display: {
+      type: Boolean,
+      required: true,
+    },
+  },
+});
+</script>
